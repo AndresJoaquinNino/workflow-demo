adc8a09a865271953055824a944f4aea73ad29b6
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-04 20:28:13 -0400

    Setup chakra ui

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
 import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>,
 )
