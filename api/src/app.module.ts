8ca32a6e80303f7c6374a487de7cfeafa17f3dcc
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-08 11:33:43 -0400

    feat: setup environment file config

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_PATH } from './config/constants';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV_PATH,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
