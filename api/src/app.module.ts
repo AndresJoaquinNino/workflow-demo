3952c274ba007afa2b518e7ec8d7c4a225e39326
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-08 14:41:53 -0400

    style: use correct indentation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,23 +4,23 @@ import { ENV_PATH } from './config/constants';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true,
-            envFilePath: ENV_PATH,
-        }),
-        TypeOrmModule.forRoot({
-            type: 'mysql',
-            host: process.env.MYSQL_HOST,
-            port: parseInt(process.env.MYSQL_PORT, 10),
-            username: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASSWORD,
-            database: process.env.MYSQL_NAME,
-            synchronize: true,
-            autoLoadEntities: true,
-        }),
-    ],
-    controllers: [],
-    providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV_PATH,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT, 10),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
