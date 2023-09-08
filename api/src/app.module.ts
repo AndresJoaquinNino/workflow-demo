f40ac2f9e4c815b51702f194ff1124575474bc82
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-08 14:22:41 -0400

    style: format main and app files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,13 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { ENV_PATH } from './config/constants';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: ENV_PATH,
-    }),
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ENV_PATH,
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
