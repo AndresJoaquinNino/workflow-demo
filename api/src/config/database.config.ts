275c222517e67f054eae711afa7cd2c74a753a27
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-10 11:23:44 -0400

    revert: db synchronize option

--- api/src/config/database.config.ts
+++ api/src/config/database.config.ts
@@ -8,7 +8,7 @@ export default registerAs('database', () => ({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   logging: true,
-  synchronize: false,
+  synchronize: true,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   migrationsTableName: 'migrations',
