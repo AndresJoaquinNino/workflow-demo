9fb1049a5f24b303d354bf993547ecb1e9b5658c
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-10 12:12:41 -0400

    feat: list all node types

--- api/src/node/node.controller.ts
+++ api/src/node/node.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { NodeService } from './node.service';
+import { NodeType } from './entities/node-type.entity';
 
 @Controller('node')
 export class NodeController {
   constructor(private readonly nodeService: NodeService) {}
 
   @Get('types')
-  findAllNodeTypes() {
+  @HttpCode(HttpStatus.OK)
+  async findAllNodeTypes(): Promise<NodeType[]> {
     return this.nodeService.findAllNodeTypes();
   }
 }
--- api/src/node/node.service.ts
+++ api/src/node/node.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NodeType } from './entities/node-type.entity';
 
 @Injectable()
 export class NodeService {
-  findAllNodeTypes() {
-    return `This action returns all node types`;
+  constructor(
+    @InjectRepository(NodeType)
+    private nodeTypeRepository: Repository<NodeType>,
+  ) {}
+
+  findAllNodeTypes(): Promise<NodeType[]> {
+    return this.nodeTypeRepository.find({
+      relations: ['nodeShape'],
+    });
   }
 }
