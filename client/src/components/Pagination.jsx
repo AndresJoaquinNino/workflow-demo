b22acaa9e83752bd8f62bef22adf36194c25dc40
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-11 11:19:02 -0400

    feat: create pagination component

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,64 @@
+import { Button, HStack } from '@chakra-ui/react';
+import PropTypes from 'prop-types';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+
+  const MAX_PAGE_BUTTONS = 5;
+
+  const generatePageButtons = () => {
+    const pageButtons = [];
+    let buttonCount = 1;
+    for (let page = 1; page <= MAX_PAGE_BUTTONS; page++) {
+      buttonCount = buttonCount++;
+
+      let buttonsProps = {
+        key: page,
+        variant: currentPage === page ? 'solid' : 'outline',
+        colorScheme: currentPage === page ? 'blue' : 'gray',
+        onClick: page <= totalPages
+          ? () => onPageChange(page)
+          : () => { },
+      }
+
+      pageButtons.push(
+        <Button {...buttonsProps}>
+          {page <= totalPages ? page : ''}
+        </Button>
+      )
+    }
+
+    return pageButtons;
+  };
+
+  return (
+    <HStack width='auto' justify="center" gap={3}>
+      <Button
+        isDisabled={currentPage === 1}
+        onClick={() => onPageChange(currentPage - 1)}
+        mr={2}
+      >
+        Anterior
+      </Button>
+      <HStack gap={1}>
+        {
+          generatePageButtons()
+        }
+      </HStack>
+      <Button
+        isDisabled={currentPage === totalPages}
+        onClick={() => onPageChange(currentPage + 1)}
+        ml={2}
+      >
+        Siguiente
+      </Button>
+    </HStack>
+  );
+};
+
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+};
+
+export default Pagination;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export * from './Nodes';
 export { default as Navbar } from './Navbar.jsx';
 export { default as WorkflowLayout } from './WorkflowLayout.jsx';
+export { default as Pagination } from './Pagination.jsx';
