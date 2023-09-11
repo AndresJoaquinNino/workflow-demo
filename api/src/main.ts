4ecad44e975ca855bae56fa544592534703fe4c2
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-11 00:47:35 -0400

    feat: enable api cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(process.env.API_PORT);
 }
