d6be45995bd3426595295a1870264bd7b906048c
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-11 12:47:27 -0400

    feat: implement notification context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import router from './router';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { reactQueryClient } from './config';
+import { NotificationProvider } from './context';
 import 'reactflow/dist/style.css';
 
 
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={reactQueryClient}>
       <ChakraProvider>
-        <RouterProvider router={router} />
+        <NotificationProvider>
+          <RouterProvider router={router} />
+        </NotificationProvider>
       </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>,
