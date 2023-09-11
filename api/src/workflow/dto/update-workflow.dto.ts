db513280665b354d1be36670c05a1a623aeaf71c
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-10 21:31:18 -0400

    feat: add nodes and edges for update workflow

--- api/src/workflow/dto/update-workflow.dto.ts
+++ api/src/workflow/dto/update-workflow.dto.ts
@@ -1,7 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateWorkflowDto } from './create-workflow.dto';
+import { Node } from 'src/node/entities/node.entity';
+import { Edge } from 'src/edge/entities/edge.entity';
 
 export class UpdateWorkflowDto extends PartialType(CreateWorkflowDto) {
   name?: string;
   description?: string;
+  nodes?: Node[];
+  edges?: Edge[];
 }
