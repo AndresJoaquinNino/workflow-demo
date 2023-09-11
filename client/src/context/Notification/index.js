1b36f1269e7ab4d0dde0d5880185db385269cf67
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-11 13:59:53 -0400

    fix: type on useNotificationContext import

--- client/src/context/Notification/index.js
+++ client/src/context/Notification/index.js
@@ -1,3 +1,3 @@
 export { default as NotificationContext } from './NotificationContext.js';
 export { default as NotificationProvider } from './NotificationProvider.jsx';
-export { default as useNotification } from './useNotificationContext.js';
+export { default as useNotificationContext } from './useNotificationContext.js';
