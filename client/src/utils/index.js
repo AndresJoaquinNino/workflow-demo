24ee6e09e4fc266f0cd67a31689592899e1e76bb
AndresJoaquinNino <andresninorodil@gmail.com>
2023-09-11 09:30:13 -0400

    feat: create arrayFiller util function

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -9,3 +9,11 @@ export const COMPONENT_HEIGHT = Object.freeze({
   navbar: '64px',
   sidebarWorkflow: 'calc(100vh - 100px)',
 });
+
+export const arrayFiller = (array, quantity, value) => {
+  const newArray = [];
+  for(let i = array.length; i < quantity; i++){
+    newArray.push(value);
+  }
+  return [...array, ...newArray];
+}
